012e2ab9e0a85aa10afd63ec04719e7bec29fbbc
DFong298 <dennis1.fong@gmail.com>
2023-01-03 00:03:38 -0500

    landing page (mostly) done

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+let _name = document.getElementById('name');
+let about = document.getElementById('about-wrapper');
 let portrait = document.getElementById('portrait');
-let foreground = document.getElementById('foreground');
 let background = document.getElementById('background');
-let lighting = document.getElementById('lighting');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-    portrait.style.marginTop = value * 3 + 'px';
-    background.style.top = value * 0.75 + 'px'
+    _name.style.top = value * 0.75 + 'px';
+    background.style.top = value * 0.75 + 'px';
+    about.style.top = value * 0.8 + 'px';
+    portrait.style.top = value * 1 + 'px';
 })
 
