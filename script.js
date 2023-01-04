ca1fbc0738847e6d71b4cb77da65158323c77744
DFong298 <dennis1.fong@gmail.com>
2023-01-04 18:49:38 -0500

    added favicon and temporary projects section

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 let _name = document.getElementById('name');
-let about = document.getElementById('about-wrapper');
-let portrait = document.getElementById('portrait');
+let about = document.getElementById('about-content');
 let background = document.getElementById('background');
-//let links = document.getElementById('links')
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-    _name.style.top = value * 0.75 + 'px';
+    _name.style.top = value * 0.8 + 'px';
     background.style.top = value * 0.75 + 'px';
     about.style.top = value * 1 + 'px';
-    portrait.style.top = value * 1 + 'px';
-    //links.style.top = value * 1.2 + 'px'
 })
 
