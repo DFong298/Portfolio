6e91caf97a2cfdd60655775cc87e00315573d11d
DFong298 <dennis1.fong@gmail.com>
2023-06-02 18:06:19 -0400

    revamped code structure

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let _name = document.getElementById('name');
 let about = document.getElementById('about-content');
 let background = document.getElementById('background');
 
-window.addEventListener('scroll', () => {
+/*window.addEventListener('scroll', () => {
     let value = window.scrollY;
     _name.style.top = value * 0.8 + 'px';
     background.style.top = value * 0.75 + 'px';
     about.style.top = value * 1 + 'px';
-})
+})*/
 
